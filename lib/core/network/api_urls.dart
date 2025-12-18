2faeba259147e20bb4cc6f7916f9dd411bd2c1c3
pola-13 <pola.thabet@seitech-solutions.com>
2025-12-18 19:59:28 +0200

    add the new base url with HTTPS

--- lib/core/network/api_urls.dart
+++ lib/core/network/api_urls.dart
@@ -1,11 +1,13 @@
 class ApiUrls {
+  static String get baseUrl => "https://146.190.156.89";
 
-  static String get baseUrl =>"https://146.190.156.89";
+  static String login = "/api/auth/mobile/v2/login/";
+  static String signup = "/api/auth/mobile/v2/signup/";
+  static String stations = "/api/mobile/v2/stations/";
+  static String userProfile = "/api/auth/mobile/v2/me/";
+  static String logout = "/api/auth/mobile/v2/logout/";
 
-  static String  login ="/api/auth/mobile/v2/login/";
-  static String  signup ="/api/auth/mobile/v2/signup/";
-  static String  stations ="/api/mobile/v2/stations/";
-  static String  userProfile ="/api/auth/mobile/v2/me/";
-  static String  logout ="/api/auth/mobile/v2/logout/";
+  static String country = "/api/mobile/v2/reference/countries/";
+  static String city = "/api/mobile/v2/reference/cities/";
+  static String district = "/api/mobile/v2/reference/districts/";
 }
-
